62517d0e19019b1bf224ef747731312b0bfe0f97
andreas <andreaswijaya89@yahoo.com>
2012-08-27 08:29:39 +0700

    update facetly

--- config.js.php
+++ config.js.php
@@ -1,16 +1,17 @@
 <?php
-include('../../config/config.inc.php');
-$consumer_key = Configuration::get('facetly_consumer_key');
-$api_server   = Configuration::get('facetly_server_name');
-$limit        = Configuration::get('facetly_search_limit');
-$add_var      = Configuration::get('facetly_additional_variable');
+	include('../../config/config.inc.php');
+	$consumer_key = Configuration::get('facetly_consumer_key');
+	$api_server = Configuration::get('facetly_server_name');
+	$limit = Configuration::get('facetly_search_limit');
+	$add_var = Configuration::get('facetly_additional_variable');
 ?>
 
-                    
+					
 var facetly = {
-    "key" : "<?php echo $consumer_key; ?>",
-    "server" : "<?php echo $api_server; ?>",
-    "file" : "modules/facetly/find.php?<?php echo $add_var; ?>",
-    "baseurl" : "/",
-    "limit" : <?php echo $limit; ?>
+	"key" : "<?php echo $consumer_key ?>",
+	"server" : "<?php echo $api_server ?>",
+	"file" : "modules/facetly/find.php?<?php echo $add_var ?>",
+	"baseurl" : "<?php echo __PS_BASE_URI__ ?>",
+	"limit" : <?php echo $limit ?>
 }
+				
